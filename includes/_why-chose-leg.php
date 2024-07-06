390f3be5c31e93ad32eb5a5b34efe9f96cc247dd
Usama Husain <nhusain34@gmail.com>
2024-07-06 11:30:36 +0530

    Icons Modified

--- includes/_why-chose-leg.php
+++ includes/_why-chose-leg.php
@@ -7,7 +7,8 @@
         <div class="row p-0">
             <div class="col-lg-4 p-5">
                 <div class="card border-0 bg-none">
-                    <i class="fa-solid fa-shield card-big-icon text-center"></i>
+                    <img class="card-big-icon" src="<?php echo SITE_PATH?>assets/icons/trusted-agency.svg" alt="Icon Trust">
+                    <!-- <i class="fa-solid fa-shield card-big-icon text-center"></i> -->
                     <div class="card-body text-center">
                         <h5 class="card-title">Trusted UK Agency</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quae quo eligendi? Voluptatum, porro odit!</p>
@@ -16,7 +17,8 @@
             </div>
             <div class="col-lg-4 p-5">
                 <div class="card border-0 bg-none">
-                    <i class="fa-solid fa-music card-big-icon text-center"></i>
+                    <img class="card-big-icon" src="<?php echo SITE_PATH?>assets/icons/best-talent.svg" alt="Icon Trust">
+                    <!-- <i class="fa-solid fa-music card-big-icon text-center"></i> -->
                     <div class="card-body text-center">
                     <h5 class="card-title">The Best Talent For Your Event</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quae quo eligendi? Voluptatum, porro odit!</p>
@@ -25,7 +27,8 @@
             </div>
             <div class="col-lg-4 p-5">
                 <div class="card border-0 bg-none">
-                    <i class="fa-regular fa-user card-big-icon text-center"></i>
+                    <img class="card-big-icon" src="<?php echo SITE_PATH?>assets/icons/reliable-customer-care.svg" alt="Icon Trust">
+                    <!-- <i class="fa-regular fa-user card-big-icon text-center"></i> -->
                     <div class="card-body text-center">
                         <h5 class="card-title">Reliable Customer Care</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quae quo eligendi? Voluptatum, porro odit!</p>
